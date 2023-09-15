61ec9091e563b402644277b1db8cec514adc2548
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2023-09-15 15:32:47 +0530

    Changer button added + Global variable learnt!

--- script1.js
+++ script1.js
@@ -1,19 +1,21 @@
 function colour() {
      
     setInterval(function() {
- 
-        if (document.getElementById('var').style.color == 'black')
-            var col1 = 'blue';
-        else
-            col1 = 'black';
+        if (colorChanger){
+            if (document.getElementById('var').style.color == 'black')
+                var col1 = 'orange';
+            else
+                col1 = 'black';
  
         // setting value of color 1 through var
 
-        document.getElementById('var').style.color = col1;
-        console.log("Changed Welcome!")
+            document.getElementById('var').style.color = col1;
+        }
+        
+        // console.log("Changed Welcome!")
         // changing color of h1 in html
     }, 10);
  
 }
 
-colour(); 
+colour();
--- script2.js
+++ script2.js
@@ -1,16 +1,18 @@
 function colour() {
      
     setInterval(function() {
- 
-        if (document.getElementById('got').style.color == 'black')
-            var col1 = 'green';
-        else
-            col1 = 'black';
+        if (colorChanger){
+            if (document.getElementById('got').style.color == 'black')
+                var col1 = 'lightblue';
+            else
+                col1 = 'black';
  
         // setting value of color 1 through var
 
-        document.getElementById('got').style.color = col1;
-        console.log("Changed Bro!")
+            document.getElementById('got').style.color = col1;
+        }
+        
+        // console.log("Changed Bro!")
         // changing color of h1 in html
     }, 10);
  
